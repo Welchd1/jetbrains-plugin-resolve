c2eb889fe16513b55bd0960e9e889f54fc2d571e
dtwelch <dtw.welch@gmail.com>
2017-02-13 15:48:15 -0500

    finish updating symbol completion glyphs. Now need to update the gui..

--- src/edu/clemson/resolve/jetbrains/completion/MathSymbolCompletionContributor.java
+++ src/edu/clemson/resolve/jetbrains/completion/MathSymbolCompletionContributor.java
@@ -130,8 +130,7 @@ public class MathSymbolCompletionContributor extends CompletionContributor {
         SYMBOL_MAP.put("rho", "ρ");
         SYMBOL_MAP.put("sigma", "σ");
         SYMBOL_MAP.put("tau", "τ");
-        SYMBOL_MAP.put("phi", "φ");
-        SYMBOL_MAP.put("varphi", "φ");
+        SYMBOL_MAP.put("phi", "ϕ");
         SYMBOL_MAP.put("varphi", "φ");
         SYMBOL_MAP.put("chi", "χ");
         SYMBOL_MAP.put("psi", "ψ");
@@ -156,6 +155,99 @@ public class MathSymbolCompletionContributor extends CompletionContributor {
         SYMBOL_MAP.put("Int", "ℤ");
         SYMBOL_MAP.put("Powerset", "℘");
 
+        //Logic
+        SYMBOL_MAP.put("wedge", "∧");
+        SYMBOL_MAP.put("bigwedge", "⋀");
+        SYMBOL_MAP.put("vee", "∨");
+        SYMBOL_MAP.put("bigvee", "⋁");
+        SYMBOL_MAP.put("neg", "¬");
+
+        //Fundamental
+        SYMBOL_MAP.put("Forall", "∀");
+        SYMBOL_MAP.put("Exists", "∃");
+        SYMBOL_MAP.put("vdash", "⊢");
+
+        //Punctuation and Brackets
+        SYMBOL_MAP.put("langle", "⟨");
+        SYMBOL_MAP.put("rangle", "⟩");
+        SYMBOL_MAP.put("lceil", "⌈");
+        SYMBOL_MAP.put("rceil", "⌉");
+        SYMBOL_MAP.put("lcup", "⎝");
+        SYMBOL_MAP.put("rcup", "⎠");
+        SYMBOL_MAP.put("dblbar", "∥");
+
+        //Operators
+        SYMBOL_MAP.put("cap", "∩");
+        SYMBOL_MAP.put("bigcap", "⋂");
+        SYMBOL_MAP.put("cup", "∪");
+        SYMBOL_MAP.put("bicup", "⋃");
+        SYMBOL_MAP.put("sqcup", "⊔");
+        SYMBOL_MAP.put("bigsqcup", "⨆");
+        SYMBOL_MAP.put("sqcap", "⊓");
+        SYMBOL_MAP.put("bigsqcap", "⨅");
+        SYMBOL_MAP.put("propto", "∝");
+        SYMBOL_MAP.put("uplus", "⊎");
+        SYMBOL_MAP.put("biguplus", "⨄");
+        SYMBOL_MAP.put("pm", "±");
+        SYMBOL_MAP.put("mp", "∓");
+        SYMBOL_MAP.put("times", "×");
+        SYMBOL_MAP.put("div", "÷");
+        SYMBOL_MAP.put("cdot", "⋅");
+        SYMBOL_MAP.put("star", "⋆");
+        SYMBOL_MAP.put("bullet", "∙");
+        SYMBOL_MAP.put("circ", "∘");
+        SYMBOL_MAP.put("oplus", "⊕");
+        SYMBOL_MAP.put("bigoplus", "⨁");
+        SYMBOL_MAP.put("bigoplus", "⊗");
+        SYMBOL_MAP.put("bigotimes", "⨂");
+        SYMBOL_MAP.put("odot", "⊙");
+        SYMBOL_MAP.put("bigodot", "⨀");
+        SYMBOL_MAP.put("ominus", "⊖");
+        SYMBOL_MAP.put("sum", "∑");
+        SYMBOL_MAP.put("prod", "∏");
+        SYMBOL_MAP.put("prod", "∏");
+        SYMBOL_MAP.put("coprod", "∐");
+        SYMBOL_MAP.put("bowtie", "⋈");
+        SYMBOL_MAP.put("ltimes", "⋉");
+        SYMBOL_MAP.put("rtimes", "⋊");
+        SYMBOL_MAP.put("boxtimes", "⊠");
+        SYMBOL_MAP.put("blacksquare", "∎");
+        SYMBOL_MAP.put("multimap", "⊸");
+        SYMBOL_MAP.put("multimap", "⊸");
+
+        //Relations
+        SYMBOL_MAP.put("leq", "≤");
+        SYMBOL_MAP.put("geq", "≥");
+        SYMBOL_MAP.put("neq", "≠");
+        SYMBOL_MAP.put("ll", "≪");
+        SYMBOL_MAP.put("gg", "≫");
+        SYMBOL_MAP.put("lesssim", "≲");
+        SYMBOL_MAP.put("gtrsim", "≳");
+        SYMBOL_MAP.put("lessapprox", "⪅");
+        SYMBOL_MAP.put("gtrapprox", "⪆");
+        SYMBOL_MAP.put("in", "∈");
+        SYMBOL_MAP.put("notin", "∉");
+        SYMBOL_MAP.put("subset", "⊂");
+        SYMBOL_MAP.put("supset", "⊃");
+        SYMBOL_MAP.put("subseteq", "⊆");
+        SYMBOL_MAP.put("supseteq", "⊇");
+        SYMBOL_MAP.put("sqsubset", "⊏");
+        SYMBOL_MAP.put("sqsupset", "⊐");
+        SYMBOL_MAP.put("sqsubseteq", "⊑");
+        SYMBOL_MAP.put("sqsupseteq", "⊒");
+        SYMBOL_MAP.put("sim", "∼");
+        SYMBOL_MAP.put("doteq", "≐");
+        SYMBOL_MAP.put("simeq", "≃");
+        SYMBOL_MAP.put("approx", "≈");
+        SYMBOL_MAP.put("asymp", "≍");
+        SYMBOL_MAP.put("cong", "≅");
+        SYMBOL_MAP.put("equiv", "≡");
+        SYMBOL_MAP.put("preccurlyeq", "≼");
+        SYMBOL_MAP.put("succcurlyeq", "≽");
+        SYMBOL_MAP.put("triangleleft", "⊲");
+        SYMBOL_MAP.put("triangleright", "⊳");
+        SYMBOL_MAP.put("trianglelefteq", "⊴");
+        SYMBOL_MAP.put("trianglerighteq", "⊵");
     }
 
     public boolean invokeAutoPopup(@NotNull PsiElement position, char typeChar) {
