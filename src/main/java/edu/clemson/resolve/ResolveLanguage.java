e8467515364858bfbfc150c0216879854cf040d1
dtwelch <dtw.welch@gmail.com>
2019-12-30 19:08:41 -0500

    seems to work; move over the module and library sdk code (and hope it works :-)

--- src/main/java/edu/clemson/resolve/ResolveLanguage.java
+++ src/main/java/edu/clemson/resolve/ResolveLanguage.java
@@ -5,6 +5,8 @@ import com.intellij.openapi.fileTypes.LanguageFileType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.swing.*;
+
 public class ResolveLanguage extends Language {
     public static final ResolveLanguage INSTANCE = new ResolveLanguage();
 
@@ -27,5 +29,41 @@ public class ResolveLanguage extends Language {
     @Override
     public boolean isCaseSensitive() {
         return true;
+    }
+
+    /**
+     * The {@link LanguageFileType} for RESOLVE files.
+     */
+    public static class ResolveFileType extends LanguageFileType {
+
+        public static final ResolveFileType INSTANCE = new ResolveFileType();
+
+        private ResolveFileType() {
+            super(ResolveLanguage.INSTANCE);
+        }
+
+        @NotNull
+        @Override
+        public String getName() {
+            return "RESOLVE";
+        }
+
+        @NotNull
+        @Override
+        public String getDescription() {
+            return "a RESOLVE file";
+        }
+
+        @NotNull
+        @Override
+        public String getDefaultExtension() {
+            return "resolve";
+        }
+
+        @Nullable
+        @Override
+        public Icon getIcon() {
+            return ResolveIcons.FILE;
+        }
     }
 }
